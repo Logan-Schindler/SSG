7ad918a91bb95c0fe5c05cc2cb21c71dc19b3b55
Logan-Schindler <lschindler@ceg1.com>
2026-03-10 18:09:17 -0500

    Prep for Github

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from textnode import TextNode
 from pathlib import Path
@@ -30,7 +31,7 @@ def copy_files_in_directory(src, dest):
             copy_files_in_directory(old_path, new_path)
     return
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as f:
         from_markdown = f.read()
@@ -40,11 +41,13 @@ def generate_page(from_path, template_path, dest_path):
     content_title = extract_title(from_markdown)
     template_html = template_html.replace('{{ Title }}', content_title)
     template_html = template_html.replace('{{ Content }}', content_html)
+    template_html = template_html.replace('href="/',f'href="{basepath}')
+    template_html = template_html.replace('src="/',f'src"={basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as f:
         f.write(template_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     files = os.listdir(dir_path_content)
     for file in files:
         old_path = os.path.join(dir_path_content, file)
@@ -52,15 +55,16 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(old_path):
             # print(f'Add file: {new_path}')
             new_path_html = os.path.join(dest_dir_path, 'index.html')
-            generate_page(old_path, template_path, new_path_html) 
+            generate_page(basepath, old_path, template_path, new_path_html) 
         else:
             # print(f'Add directory: {new_path}')
             os.mkdir(new_path)
-            generate_pages_recursive(old_path, template_path, new_path)
+            generate_pages_recursive(basepath, old_path, template_path, new_path)
 
 def main(): 
-    copy_directory_to_dest('/home/lschindler/projects/github.com/bootdev/SSG/static','/home/lschindler/projects/github.com/bootdev/SSG/public')
+    basepath = sys.argv[0] if len(sys.argv) > 0 else '/'  
+    copy_directory_to_dest('/home/lschindler/projects/github.com/bootdev/SSG/static','/home/lschindler/projects/github.com/bootdev/SSG/docs')
     # generate_page('/home/lschindler/projects/github.com/bootdev/SSG/content/index.md','/home/lschindler/projects/github.com/bootdev/SSG/template.html','/home/lschindler/projects/github.com/bootdev/SSG/public/index.html')
-    generate_pages_recursive('/home/lschindler/projects/github.com/bootdev/SSG/content','/home/lschindler/projects/github.com/bootdev/SSG/template.html','/home/lschindler/projects/github.com/bootdev/SSG/public')
+    generate_pages_recursive(basepath, '/home/lschindler/projects/github.com/bootdev/SSG/content','/home/lschindler/projects/github.com/bootdev/SSG/template.html','/home/lschindler/projects/github.com/bootdev/SSG/docs')
 
 main()
